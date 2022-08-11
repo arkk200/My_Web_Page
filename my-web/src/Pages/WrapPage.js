c222c9a7981075b571bf4a5359ac4b2d0885c403
arkk200 <arkk20000@gmail.com>
2022-08-11 14:11:31 +0900

    Added smooth scroll

--- my-web/src/Pages/WrapPage.js
+++ my-web/src/Pages/WrapPage.js
@@ -1,16 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import FirstPage from "./FirstPage/FirstPage";
 import './WrapPage.css';
 
 function WrapPage(){
+    const wrapPage = useRef();
+    
+    const preventScroll = e => {window.scrollTo(0, 0)}
+    window.addEventListener('scroll', preventScroll);
+    setTimeout(() => {
+        window.removeEventListener('scroll', preventScroll);
+    }, 2000);
+
+    let wrapYSize = 0;
     useEffect(() => {
         function animate(){
             requestAnimationFrame(animate);
+            wrapYSize += (window.scrollY - wrapYSize) / 20;
+            wrapPage.current.style.top = `-${wrapYSize}px`;
         }
         animate();
     }, []);
     return(
-        <div id="wrap-page">
+        <div id="wrap-page" ref={wrapPage}>
             <FirstPage />
         </div>
     );
