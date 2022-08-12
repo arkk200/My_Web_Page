22bf36b48f2337fec0f41d55429273ad6666c6b5
arkk200 <arkk20000@gmail.com>
2022-08-13 08:56:41 +0900

    Finished PageGuide

--- my-web/src/PageGuide.js
+++ my-web/src/PageGuide.js
@@ -3,17 +3,31 @@ import { useRef } from 'react';
 function PageGuide(){
     const guideGauge = useRef();
     window.addEventListener('scroll', () => {
-        guideGauge.current.style.height = `${300 * window.scrollY / (window.innerHeight * 6)}px`
+        let gaugeLength = 0;
+        if(window.scrollY < window.innerHeight * 2){
+            gaugeLength = window.scrollY / (window.innerHeight * 3) <= 1 / 3 ?
+                window.scrollY / (window.innerHeight * 3)
+                : 1 / 3;
+        } else if (window.scrollY < window.innerHeight * 4){
+            gaugeLength = (window.scrollY - window.innerHeight) / (window.innerHeight * 3) <= 2 / 3 ?
+            (window.scrollY - window.innerHeight) / (window.innerHeight * 3)
+                : 2 / 3;
+        } else {
+            gaugeLength = (window.scrollY - window.innerHeight * 2) / (window.innerHeight * 3) <= 1 ?
+            (window.scrollY - window.innerHeight * 2) / (window.innerHeight * 3)
+                : 1;
+        }
+        guideGauge.current.style.height = `${300 * gaugeLength}px`;
     })
     return(
         <div id="page-guide">
             <div id="guide-line">
                 <div id="guide-gauge" ref={guideGauge}></div>
+                <div className="page-point"></div>
+                <div className="page-point"></div>
+                <div className="page-point"></div>
+                <div className="page-point"></div>
             </div>
-            <div id="fist-page-point"></div>
-            <div id="second-page-point"></div>
-            <div id="third-page-point"></div>
-            <div id="fouth-page-point"></div>
         </div>
     )
 }
