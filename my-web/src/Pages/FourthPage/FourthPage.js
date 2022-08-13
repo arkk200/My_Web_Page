6ccd03efb60c6665f72ba85bbb3f700329eb3eff
arkk200 <arkk20000@gmail.com>
2022-08-13 17:52:09 +0900

    Finished Fourth Page

--- my-web/src/Pages/FourthPage/FourthPage.js
+++ my-web/src/Pages/FourthPage/FourthPage.js
@@ -1,10 +1,15 @@
 import './FourthPage.css';
+import brace_image from '../../Images/brace.png';
 
-function FourthPage(){
-    return(
+function FourthPage() {
+    return (
         <div id="fourth-page">
-            <div id="fourth-page-contents">
-                test4
+            <div id="fourth-contents">
+                <h2 id="fourth-title">My works</h2>
+                <div className="works">
+                    <div id="three" className="work-titles">Three.js</div>
+                </div>
+                <img src={brace_image} id="fourth-brace" />
             </div>
         </div>
     );
