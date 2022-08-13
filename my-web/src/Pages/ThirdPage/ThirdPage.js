34075517842f85ee4f8ab5bec220e006dceeb276
arkk200 <arkk20000@gmail.com>
2022-08-13 13:05:39 +0900

    Finished Third Page

--- my-web/src/Pages/ThirdPage/ThirdPage.js
+++ my-web/src/Pages/ThirdPage/ThirdPage.js
@@ -1,10 +1,15 @@
 import './ThirdPage.css';
+import brace_image from '../../Images/brace.png';
 
 function ThirdPage(){
     return(
         <div id="third-page">
-            <div id="third-page-contents">
-                test3
+            <div id="third-contents">
+                <h2 id="third-title">My stacks</h2>
+                <img className="third-brace" src={brace_image} />
+                <img className="third-brace" src={brace_image} />
+                <img className="third-brace" src={brace_image} />
+                <img className="third-brace" src={brace_image} />
             </div>
         </div>
     );
