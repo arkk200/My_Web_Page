d24038594183aa0cf9e001a44e2ce276992ea68d
arkk200 <arkk20000@gmail.com>
2022-08-16 10:21:06 +0900

    Removed second paragraph

--- my-web/src/Pages/SecondPage/SecondPage.js
+++ my-web/src/Pages/SecondPage/SecondPage.js
@@ -5,12 +5,6 @@ function SecondPage(){
         <div id="second-page">
             <div id="second-contents">
                 <h2 id="second-title">My favorite</h2>
-                <div id="second-paragraph">
-                    리듬게임을 하거나
-                    awwwards에 있는 웹사이트를 구경하거나<br />
-                    Three.js나 캔버스로
-                    3D 오브젝트를 만들거나 그리는 것을 좋아합니다
-                </div>
             </div>
         </div>
     );
